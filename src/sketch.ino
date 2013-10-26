12b4f6ad43f5336b0cb5e914a1e6599345dbd2a8
Michael Witrant <michael@ouvrages-web.fr>
2013-10-26 16:27:35 +0200

    blink all

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,8 +1,23 @@
 
 void setup()
 {
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
 void loop()
 {
+  int i;
+  for (i=0; i<6; i++) {
+    digitalWrite(i,HIGH);
+  }
+  delay(300);
+  for (i=0; i<6; i++) {
+    digitalWrite(i,LOW);
+  }
+  delay(300);
 }
