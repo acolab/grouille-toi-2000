f819ecc92bdd3f12e141a4a6edc2474bc855c944
Michael Witrant <michael@ouvrages-web.fr>
2013-10-26 20:56:34 +0200

    working

--- gt2k.rb
+++ gt2k.rb
@@ -1,36 +1,93 @@
 require 'net/http'
 require 'time'
 
-url = 'http://data.keolis-rennes.com/xml/'
-query = {
-  cmd: 'getbusnextdepartures',
-  version: '2.1',
-  key: 'RK8BUNJ7NSYTNFU',
-  #param: {
-  #  mode: 'stopline',
-  #  route: [0077],
-  #  direction: [0],
-  #  stop: [3115],
-  #}
-  "param[mode]" => 'stop',
-  "param[stop][]" => "2208",
-}
-uri = URI(url)
-uri.query = URI.encode_www_form(query)
-response = Net::HTTP.get_response(uri)
+def get_delay
+  url = 'http://data.keolis-rennes.com/xml/'
+  query = {
+    cmd: 'getbusnextdepartures',
+    version: '2.1',
+    key: 'RK8BUNJ7NSYTNFU',
+    #param: {
+    #  mode: 'stopline',
+    #  route: [0077],
+    #  direction: [0],
+    #  stop: [3115],
+    #}
+    "param[mode]" => 'stop',
+    "param[stop][]" => "2208",
+  }
+  uri = URI(url)
+  uri.query = URI.encode_www_form(query)
+  response = Net::HTTP.get_response(uri)
 
-puts response.body
+  #puts response.body
 
-if response.body =~ /localdatetime="(.+?)"/
-  now = Time.parse($1)
+  if response.body =~ /localdatetime="(.+?)"/
+    now = Time.parse($1)
+  end
+
+  if response.body =~ /<departure.*?>(.+?)<\/departure>/
+    time = $1
+    puts time
+    time = Time.parse(time)
+  end
+  time - now
 end
 
-if response.body =~ /<departure.*?>(.+?)<\/departure>/
-  time = $1
-  time = Time.parse(time)
+def set_color(r,g,b)
+  serial = File.open("/dev/ttyUSB0", "r+")
+  command = [r,g,b].join(",")
+  p command
+  serial.puts(command) 
+  serial.close
 end
-remaining = time - now
-minutes = remaining / 60.0
-puts minutes
 
+def button_pressed?
+  true
+end
 
+Infinity = Float::INFINITY
+
+def run_alarm
+  last_update = nil
+  arrival = nil
+  loop do
+    now = Time.now
+    if last_update.nil? or now >= last_update + 30
+      puts "update"
+      arrival = now + get_delay
+      last_update = now
+    end
+
+    minutes = (arrival - now) / 60.0
+    p minutes: minutes
+
+    case minutes
+    when 20.0..Infinity
+      color = [0,0,1]
+    when 10.0...20.0
+      nr = (minutes - 10.0)/10.0
+      r = 1.0 - nr
+      color = [0,r,nr]
+    when 0.0...10.0
+      nr = minutes/10.0
+      r = 1.0 - nr
+      color = [r, nr, 0]
+    when -Infinity..0.0
+      color = [1,0,0]
+    end
+    p color: color
+
+    color = color.map { |c| (c * 255).round }
+
+    set_color(*color)
+
+    sleep 2
+  end
+end
+
+loop do
+  if button_pressed?
+    run_alarm
+  end
+end
